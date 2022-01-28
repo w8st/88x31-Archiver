3f598659b31b107342ec410b5bf052e777189bff
Some Random Programmer <66559821+Some-RandomProgrammer@users.noreply.github.com>
2022-01-28 07:34:08 -0500

    Update Crawler.java

--- Crawler.java
+++ Crawler.java
@@ -1,7 +1,7 @@
 /*
 MIT License
 
-Copyright (c) 2021 ARandom-Programmer
+Copyright (c) 2021 Some-RandomProgrammer
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
